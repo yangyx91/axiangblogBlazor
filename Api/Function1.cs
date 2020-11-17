90887dda05f32bfa0273b1a2f1f08475faf68123
阿象 <yang.yx91@live.cn>
2020-11-17 18:22:11 +0800

    加入bing图片URL

--- Api/Function1.cs
+++ Api/Function1.cs
@@ -7,29 +7,48 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Net.Http;
+using Api.DTO;
+using System.Linq;
+using Data;
 
 namespace Api
 {
     public static class Function1
     {
+        private static string bingDailyImgApi = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN";
+
+        private static async Task<BingImg> GetBingImgAsync()
+        {
+            string result = "";
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync(bingDailyImgApi);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                result = await responseMessage.Content.ReadAsStringAsync();
+            }
+            return System.Text.Json.JsonSerializer.Deserialize<BingImgResponse>(result).images.FirstOrDefault();
+        }
+
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "products")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "todayBingImg")] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
+            var bingImg = await GetBingImgAsync();
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(bingImg);
         }
     }
 }
---
+++ Data/BingImgResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data
+{
+    public class BingImgResponse
+    {
+        public List<BingImg> images { get; set; }
+
+        //public object tooltips { get; set; }
+    }
+
+    public class BingImg
+    {
+        public string startdate { get; set; }
+        public string url { get; set; }
+        public string urlbase { get; set; }
+        public string copyright { get; set; }
+    }
+}
