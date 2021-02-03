38458bbb97f083228247f5368c99ee26f2e54eb9
阿象 <yang.yx91@live.cn>
2021-02-03 15:24:35 +0800

    集成azure signlar

---
+++ Api/Functions/ChatFunction.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using System.Threading.Tasks;
+
+namespace Api
+{
+    public static class ChatFunction
+    {
+
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Negotiate(
+            [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "chatHub")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+
+
+        [FunctionName("messages")]
+        public static Task SendMessage(
+           [HttpTrigger(AuthorizationLevel.Function, "post")] ClientMessage clientMessage,
+           [SignalR(HubName = "chatHub")] IAsyncCollector<SignalRMessage> signalRMessages)
+        {
+            return signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    Target = "clientMessage",
+                    Arguments = new[] { clientMessage }
+                });
+        }
+
+        public class ClientMessage
+        {
+            public string UID { get; set; }
+            public string Name { get; set; }
+            public string Message { get; set; }
+
+            public string CreateTime { get; set; }
+        }
+    }
+}
