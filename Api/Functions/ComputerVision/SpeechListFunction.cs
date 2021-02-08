031ee56bcf08199716f24fff3ef9c68aaa990a22
阿象 <yang.yx91@live.cn>
2021-02-08 19:02:41 +0800

    SpeechList Api

---
+++ Api/Functions/ComputerVision/SpeechListFunction.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Api.Functions.ComputerVision
+{
+    //ÎÄ±¾×ªÓïÒô Rest API (v3.1)
+    //https://docs.microsoft.com/zh-cn/azure/cognitive-services/speech-service/rest-text-to-speech#convert-text-to-speech
+    public static class SpeechListFunction
+    {
+        private static readonly string endpointkey = "f59840607fb4472bbab3e756ea4ce739";
+        private static readonly string endpointTokenURL = "https://southeastasia.api.cognitive.microsoft.com/sts/v1.0/issuetoken";
+        private static readonly string endpointList = "https://southeastasia.tts.speech.microsoft.com/cognitiveservices/voices/list";
+        private static readonly string endpointLocation = "southeastasia";
+
+       
+
+        [FunctionName("SpeechListFunction")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "speechList")] HttpRequest req)
+        {
+            using (HttpClientHandler handler = new HttpClientHandler { UseProxy = false })
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", endpointkey);
+                //client.DefaultRequestHeaders.Accept.Clear();
+                //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = await client.PostAsync(endpointList, null);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseJson = await response.Content.ReadAsStringAsync();
+                    return new OkObjectResult(responseJson);
+                }
+                else
+                {
+                    string msg = "Failure to POST. Status Code: " + response.StatusCode + ". Reason: " + response.ReasonPhrase;
+                    return new OkObjectResult(msg);
+                }
+            }
+        }
+    }
+}
---
+++ Data/Response/FaceImg_Response.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class FaceImg_Response
+    {
+        public string faceId { get; set; }
+
+        public FaceRectangle faceRectangle { get; set; }
+
+        public FaceAttributes faceAttributes { get; set; }
+    }
+
+
+    public class FaceAttributes
+    {
+        public decimal smile { get; set; }
+
+        public string gender { get; set; }
+
+        public int age { get; set; }
+
+        public FaceEmotion emotion { get; set; }
+
+        public FaceHair hair { get; set; }
+    }
+
+    public class FaceRectangle
+    {
+        public int top { get; set; }
+
+        public int left { get; set; }
+
+        public int width { get; set; }
+
+        public int height { get; set; }
+
+    }
+
+    public class FaceEmotion
+    {
+        public decimal anger { get; set; }
+
+        public decimal contempt { get; set; }
+
+        public decimal disgust { get; set; }
+
+        public decimal fear { get; set; }
+
+        public decimal happiness { get; set; }
+
+        public decimal neutral { get; set; }
+
+        public decimal sadness { get; set; }
+
+        public decimal surprise { get; set; }
+    }
+
+    public class FaceHair
+    {
+        public decimal bald { get; set; }
+
+        public bool invisible { get; set; }
+
+        public List<FaceHairColor> hairColor { get; set; }
+    }
+
+    public class FaceHairColor
+    {
+        public string color { get; set; }
+
+        public decimal confidence { get; set; }
+    }
+}
