f33d95028dbef637da32f019da9ea8d05fda2f52
阿象 <yang.yx91@live.cn>
2021-03-24 17:23:13 +0800

    集成signalr

---
+++ Api/Functions/ChatFunction.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+
+namespace Api.Functions
+{
+    public static class ChatFunction
+    {
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Negotiate(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "Chat")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+    }
+}
