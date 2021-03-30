2a7840f3a380a6c31d9f7e5b7621cba59a4b392e
阿象 <yang.yx91@live.cn>
2021-03-30 13:22:38 +0800

    编辑文章

---
+++ Data/Response/WxPost_Response.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class WxPost_Response
+    {
+        public int affectedDocs { get; set; }
+
+        public List<WxPostDocument> data { get; set; }
+    }
+
+    public class WxPostDocument
+	{
+        public string PostId { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string PostUrl { get; set; }
+		public string Author { get; set; }
+		public string PostDate { get; set; }
+		public string Tags { get; set; }
+		public string Thumbnail { get; set; }
+		public bool IsTop { get; set; }
+		public string PostType { get; set; }
+		public string Topic { get; set; }
+
+	}
+}
