d0475e6483c6026c4a5ca037009f8534905f0534
阿象 <yang.yx91@live.cn>
2021-04-13 12:11:01 +0800

    集成OSS

---
+++ Data/Response/File/UploadFile_Response.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class UploadFile_Response
+    {
+        public int affectedDocs { get; set; }
+
+        public List<WxUploadDocument> data { get; set; }
+    }
+
+
+    public class WxUploadDocument
+    {
+        public string _id { get; set; }
+        public string FileID { get; set; }
+
+        public string FileName { get; set; }
+
+        public string FileType { get; set; }
+
+        public long FileSize { get; set; }
+
+        public string UploadDate { get; set; }
+    }
+
+    public class QueryWxUploadDocumentParams
+    {
+        public int page { get; set; }
+
+        public int pageSize { get; set; }
+    }
+
+    public class DelWxUploadDocumentParams
+    {
+        public string imgUrl { get; set; }
+    }
+}
